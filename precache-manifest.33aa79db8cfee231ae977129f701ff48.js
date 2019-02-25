39713d050cfc83c229f9fdae43adf04ad50fbbb1
hjPark <cool.itea@gmail.com>
2019-02-25 23:42:38 +0900

    Updates

---
+++ precache-manifest.33aa79db8cfee231ae977129f701ff48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "6f910f3702085e22e967",
+    "url": "/movie_app/static/js/main.6f910f37.chunk.js"
+  },
+  {
+    "revision": "7e663f1ca65466bd5ee1",
+    "url": "/movie_app/static/js/2.7e663f1c.chunk.js"
+  },
+  {
+    "revision": "6f910f3702085e22e967",
+    "url": "/movie_app/static/css/main.e1800c4d.chunk.css"
+  },
+  {
+    "revision": "adc4db192d6df5ca3dd643391776c3b5",
+    "url": "/movie_app/index.html"
+  }
+];
